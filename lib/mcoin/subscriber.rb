b4bc11e5883801cacbb129e6aba29bc4b01bcc36
蒼時弦也 <elct9620@frost.tw>
2018-04-16 14:55:39 +0800

    Add support for multuple pair for subscribe

--- lib/mcoin/subscriber.rb
+++ lib/mcoin/subscriber.rb
@@ -2,10 +2,11 @@
 
 module Mcoin
   class Subscriber
-    attr_reader :markets
+    attr_reader :markets, :pairs
 
-    def initialize(picked = Market.available, type = :BTC, currency = :USD)
-      @markets = markets_from(picked, type, currency)
+    def initialize(pairs = [], market = Market.available)
+      @pairs = pairs_from(pairs)
+      @markets = markets_from(market)
     end
 
     def start(interval = 1, &block)
@@ -19,12 +20,20 @@ module Mcoin
 
     protected
 
-    def markets_from(picked, type, currency)
-      @markets ||= picked.map do |name|
-        Market
-          .pick(name)
-          .new(type, currency)
+    def pairs_from(picked)
+      @pairs ||= (picked || ['BTC-USD']).uniq.map do |pair|
+        pair.split('-').map(&:to_sym)
       end
+    end
+
+    def markets_from(picked)
+      @markets ||= picked.map do |name|
+        pairs.map do |pair|
+          Market
+            .pick(name)
+            .new(*pair)
+        end
+      end.flatten
     end
   end
 end
